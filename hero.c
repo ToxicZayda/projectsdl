53d344d41df6c614b61cbb829845417e382ca12d
ToxicZayda <61386446+ToxicZayda@users.noreply.github.com>
2020-06-17 19:48:59 +0100

    Update hero.c

--- hero.c
+++ hero.c
@@ -6,16 +6,16 @@
 void init_hero(Hero *hero)
 {
     hero->e = init_entity(100, Hero_Default_Y);
-    hero->e.sprite[0] = IMG_Load("mvt/r1.png");
-    hero->e.sprite[1] = IMG_Load("mvt/r2.png");
-    hero->e.sprite[2] = IMG_Load("mvt/r3.png");
-    hero->e.sprite[3] = IMG_Load("mvt/r4.png");
-    hero->e.sprite[4] = IMG_Load("mvt/flip.png");
-    hero->e.sprite[5] = IMG_Load("mvt/l1.png");
-    hero->e.sprite[6] = IMG_Load("mvt/l2.png");
-    hero->e.sprite[7] = IMG_Load("mvt/l3.png");
-    hero->e.sprite[8] = IMG_Load("mvt/l4.png");
-    hero->e.sprite[9] = IMG_Load("mvt/hidden.png");
+    hero->e.sprite[0] = IMG_Load("mvtt/r1.png");
+    hero->e.sprite[1] = IMG_Load("mvtt/r2.png");
+    hero->e.sprite[2] = IMG_Load("mvtt/r3.png");
+    hero->e.sprite[3] = IMG_Load("mvtt/r4.png");
+    hero->e.sprite[4] = IMG_Load("mvtt/flip.png");
+    hero->e.sprite[5] = IMG_Load("mvtt/l1.png");
+    hero->e.sprite[6] = IMG_Load("mvtt/l2.png");
+    hero->e.sprite[7] = IMG_Load("mvtt/l3.png");
+    hero->e.sprite[8] = IMG_Load("mvtt/l4.png");
+    hero->e.sprite[9] = IMG_Load("mvtt/hidden.png");
     hero->frame = 0;
     hero->direction = 'r';
     hero->mouvement.right = 0;
